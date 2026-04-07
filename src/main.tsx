88b04a1e4d72b1499cc023e764a53c3e9f560679
Elliot <elliotmobilemusic@gmail.com>
2026-04-07 22:53:41 +0100

    fix: use hash routing for desktop app

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { PreferencesProvider } from './context/PreferencesContext';
-import { hasDesktopApi } from './lib/desktop';
 import './styles.css';
-
-const Router = hasDesktopApi() ? HashRouter : BrowserRouter;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <AuthProvider>
         <PreferencesProvider>
           <App />
         </PreferencesProvider>
       </AuthProvider>
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
