0516ca0581294811d223e4ce5c0a00c3d35cc1c8
Elliot <elliotmobilemusic@gmail.com>
2026-04-08 21:51:36 +0100

    Fix infinite login page reload loop

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -34,11 +34,6 @@ export function AuthProvider({ children }: PropsWithChildren) {
     } = supabase.auth.onAuthStateChange((_event, nextSession) => {
       setSession(nextSession);
       setLoading(false);
-      
-      // If session was removed (signed out) hard refresh page to clean state
-      if (!nextSession) {
-        window.location.reload();
-      }
     });
 
     return () => {
