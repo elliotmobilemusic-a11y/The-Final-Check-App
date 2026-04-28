dc6eaf43c951994360bc7f65280bc21d3fdf5e8b
Elliot <elliotmobilemusic@gmail.com>
2026-04-28 22:20:44 +0100

    Remove unsupported stackOnTablet prop from ActionRow

--- src/components/quotes/QuoteCalculatorModule.tsx
+++ src/components/quotes/QuoteCalculatorModule.tsx
@@ -1014,7 +1014,7 @@ export function QuoteCalculatorModule({
                   </div>
                 )}
               </div>
-              <ActionRow align="end" stackOnTablet>
+              <ActionRow align="end">
                 <button type="button" className="button button-ghost" onClick={closeComposer}>
                   Cancel
                 </button>
